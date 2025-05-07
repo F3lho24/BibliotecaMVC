a7ca40fd19e985224e9a5651b26c56b2e18650e2
AndresFelipe <f3lho94@gmail.com>
2025-05-06 23:05:41 -0500

    Actualizacion Dao y Main, adicion de ConexionBD a Dao

--- src/main/java/com/biblioteca/Main.java
+++ src/main/java/com/biblioteca/Main.java
@@ -279,44 +279,7 @@ public class PanelDVDs extends JPanel {
 
 
 
-// DAO: ConexionBD.java
-package com.biblioteca.model.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-    public class ConexionBD {
-        private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
-        private static final String USER = "root";
-        private static final String PASSWORD = "password";
-
-        private static Connection instance;
-
-        private ConexionBD() {}
-
-        public static Connection getConnection() throws SQLException {
-            if (instance == null || instance.isClosed()) {
-                try {
-                    Class.forName("com.mysql.cj.jdbc.Driver");
-                    instance = DriverManager.getConnection(URL, USER, PASSWORD);
-                } catch (ClassNotFoundException e) {
-                    throw new SQLException("Driver MySQL no encontrado", e);
-                }
-            }
-            return instance;
-        }
-
-        public static void closeConnection() {
-            try {
-                if (instance != null && !instance.isClosed()) {
-                    instance.close();
-                }
-            } catch (SQLException e) {
-                System.err.println("Error al cerrar la conexión: " + e.getMessage());
-            }
-        }
-    }
 
 
 
