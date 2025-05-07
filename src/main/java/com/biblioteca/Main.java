9eb79c3d3f9a0fe1691a5679c8e4944f6cdf2d6a
AndresFelipe <f3lho94@gmail.com>
2025-05-06 23:24:15 -0500

    Creacion clase PanelDVDs.java

--- src/main/java/com/biblioteca/Main.java
+++ src/main/java/com/biblioteca/Main.java
@@ -38,23 +38,7 @@
 
 
 
-// Vista: PanelDVDs.java
-package com.biblioteca.view;
 
-import com.biblioteca.controller.DVDController;
-import com.biblioteca.model.DVD;
-import javax.swing.*;
-        import javax.swing.table.DefaultTableModel;
-import java.awt.*;
-        import java.sql.SQLException;
-import java.util.List;
-
-public class PanelDVDs extends JPanel {
-    private MainFrame parent;
-    private DVDController controller;
-
-    private JTable tablaDVDs;
-    private DefaultTableModel modeloTabla
 
 
 
